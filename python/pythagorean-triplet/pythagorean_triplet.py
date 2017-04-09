8b2fae3edddd99a078be41732ee86ddcbd4c4084
Mira <szewczyk.mira@gmail.com>
2017-04-09 19:18:58 +0200

    Triplets

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,22 +1,23 @@
-from math import gcd
+from math import gcd, sqrt
 
 
 def primitive_triplets(num):
     """Finds all primative triplets that contain a certain number"""
-    if num % 4 != 0:
+    if num % 4:
         raise ValueError
     triplets = set()
-    for (m, n) in factor_gen(num//2):
+    for (m, n) in factor_generator(num // 2):
         if gcd(m, n) == 1:
-            triplet = tuple(sorted((m**2 - n**2, 2*m*n, m**2 + n**2)))
+            triplet = tuple(sorted([m**2 - n**2, 2*m*n, m**2 + n**2]))
             triplets.add(triplet)
     return triplets
 
 
-def factor_gen(n):
+def factor_generator(n):
     """Generates factor pairs of number n"""
-    for i in range(1, int(n**0.5)+1):
-        if n % i == 0:
+    end = int(sqrt(n)) + 1
+    for i in range(1, end):
+        if not n % i:
             yield (n//i, i)
 
 
@@ -30,8 +31,10 @@ def triplets_in_range(start, stop):
                     triples.add((a, b, c),)
     return triples
 
+
 def is_pitagorean(a, b, c):
     return a**2 + b**2 == c**2
+
 
 def is_triplet(nums):
     """Is true if nums is a primative triplet"""
