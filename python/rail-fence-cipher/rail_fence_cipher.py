5491d5e4725efa38eac935b34406cb6bfa095a22
Mira <szewczyk.mira@gmail.com>
2017-06-30 22:04:45 +0200

    Rail fance cipher decoder and encoder

--- python/rail-fence-cipher/rail_fence_cipher.py
+++ python/rail-fence-cipher/rail_fence_cipher.py
@@ -1,21 +1,28 @@
+from itertools import chain
 
-def encode(text, rails):
+def fence_walk(length, rails):
     up = False
-    text = text.replace(" ", "")
     fence = [list() for _ in range(0, rails)]
     rails -= 1
     layer = 0
-    for character in text:
-        fence[layer] += character
+    for num in range(0, length):
+        fence[layer].append(num)
         if layer == rails:
             up = True
         if layer == 0:
             up = False
         layer = layer - 1 if up else layer + 1
-    return ''.join([''.join(rail) for rail in fence])
+    return chain.from_iterable(fence)
+
+def encode(text, rails):
+    text = text.replace(" ", "")
+    fence = fence_walk(len(text), rails)
+    text_dict = dict(enumerate(text))
+    return ''.join([text_dict[num] for num in fence])
     
 
-
-
 def decode(text, rails):
-    raise Exception
+    fence = fence_walk(len(text), rails)
+    text_zip = sorted(zip(fence, text), key=lambda x: x[0])
+    return ''.join([x[1] for x in text_zip])
+
