4081e4c6bb743877a4b8a4f4b9ad189f54e27acc
Mira <szewczyk.mira@gmail.com>
2017-08-05 19:46:41 +0200

    Speed improvements

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,7 +1,7 @@
 def factors(n):
-    half = int(n/2) + 1
+    square = int(n**0.5) + 1
     result = set([1])
-    for i in range(2, half):
+    for i in range(2, square):
         if n % i == 0:
             result.update([i, n//i])
     return result
