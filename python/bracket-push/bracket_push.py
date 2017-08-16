34d3ffa850933b6a1002d72e0207cfd7d2b47906
Mira <szewczyk.mira@gmail.com>
2017-08-16 22:38:41 +0200

    Stack bracket push

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -1,42 +1,20 @@
-ALLOWED = ['(', ')', '[', ']', '{', '}']
-
 CLOSURE = {
     '{': '}',
     '[': ']',
     '(': ')'
     }
 
-def remove_not_allowed(text):
-    return [i for i in text if i in ALLOWED]
-
-
-def find_closure(text, elem):
-    index = None
-    for i, c in enumerate(text):
-        if index is not None and c in CLOSURE.keys():
-            return index
-        if c == elem:
-            index = i
-    return index
-
-
-def validate_brackets(text):
-    if not text:
-        return True
-
-    first_elem = text[0]
-    closure_index = find_closure(text, CLOSURE[first_elem])
-    if not closure_index:
-        raise KeyError
-    first_half = validate_brackets(text[1:closure_index])
-    second_half = validate_brackets(text[closure_index +1:])
-    return first_half and second_half
-
-
 def check_brackets(text):
-    text = remove_not_allowed(text)
-    try:
-        return validate_brackets(text)
-    except KeyError:
-        return False
+    stack = []
+    for elem in text:
+        if elem in CLOSURE.keys():
+            stack.append(elem)
+        if elem in CLOSURE.values():
+            if not stack:
+                return False
+            if CLOSURE[stack[-1]] == elem:
+                stack.pop()
+            else:
+                return False
+    return not stack
 
